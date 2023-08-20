61efa99216a61f67058622caa4846611246cad13
javad <javad.pi799.com>
2023-08-20 13:41:58 +0330

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:0,
+    nav:true,
+    center:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3,
+            center:false
+        },
+        1200:{
+            items:4
+        }
+    }
+})
